eaf57f5027c7c553fbd311671c92073c7e779ab0
resocoy128 <127033949+resocoy128@users.noreply.github.com>
2023-03-06 13:40:18 +0200

    Create server.py

---
+++ server.py
@@ -0,0 +1,54 @@
+import socket
+from readLicensePlate import *
+import json
+import time
+import datetime
+import psycopg2
+
+DATA_INDEX = 1
+print(datetime.datetime.now())
+last_timestamp = datetime.datetime.now() - datetime.timedelta(hours=4, minutes=10)
+print(last_timestamp)
+
+def get_changes():
+    time.sleep(5)
+    connection = psycopg2.connect(
+        database="postgres",
+        user='postgres',
+        password='bagrut3',
+        host='10.252.30.4',
+        port='5432'
+    )
+
+    cursor = connection.cursor()
+
+    cursor.execute(str("SELECT * FROM sensors.images img WHERE img.sensors_timestamp > '" + str(last_timestamp) + "'"));
+    return cursor.fetchall()
+
+
+server_socket = socket.socket()
+server_socket.bind(("0.0.0.0", 8820))
+server_socket.listen()
+print("Server is up and running")
+(client_socket, client_address) = server_socket.accept()
+print("Client connected")
+
+while True:
+
+    data = get_changes()
+    for d in data:
+        print(d[3])
+        open("FromDB.jpg", 'wb').write(d[3])
+        plate = getLicensePlate()
+        # some JSON:
+        x = { "plateId":plate}
+
+        # parse x:
+        y = json.dumps(x)
+        client_socket.send(y.encode())
+        # last_timestamp = datetime.datetime.now()
+
+client_socket.close()
+server_socket.close()
+
+
