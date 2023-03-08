6dfdedb8053a96bba354f34ade10e09ced0fea14
Shragagg <127030480+Shragagg@users.noreply.github.com>
2023-03-08 09:27:23 +0200

    Update add_to_suspects_table.py

--- add_to_suspects_table.py
+++ add_to_suspects_table.py
@@ -1,10 +1,11 @@
+from socket import socket
+
 import psycopg2
 import psycopg2.extras
 from psycopg2.extensions import AsIs
 import datetime
+import random
 import requests
-import socket
-import threading
 
 
 def addToSuspectTable(report):
@@ -17,23 +18,22 @@ def addToSuspectTable(report):
     )
 
     cursor = conn.cursor(cursor_factory=psycopg2.extras.RealDictCursor)
-    cursor.execute("SELECT * FROM sensors.person WHERE '" + report['number'] + "' = any (license_plates);")
+    cursor.execute("SELECT * FROM sensors.person WHERE '" + report['number'] + "' = ANY (license_plates);")
 
     result = cursor.fetchone() # assuming every LP has one owner
-    print(result['person_id'])
+    print(result)
     if (result):
-        cursor.execute("SELECT * FROM sensors.suspects WHERE person_id = " + str(result['person_id']))
+        cursor.execute("SELECT * FROM sensors.suspects WHERE person_id = '" + str(result['person_id']) + "'")
         suspectResult = cursor.fetchone()
 
         if (suspectResult):
             update_statement = "UPDATE sensors.suspects SET sensor_id=%s, timestamp=%s, " \
-                               "sensor_location_x=%s, sensor_location_y=%s WHERE person_id = " + str(result['person_id'])
+                               "sensor_location_x=%s, sensor_location_y=%s WHERE person_id = '" + str(result['person_id'] + "'")
             cursor.execute(update_statement, (report['sensor_id'], report['timestamp'],
                                               report['sensor_location_x'], report['sensor_location_y']))
             conn.commit()
         else:
-            wantedLevel = getWantedLevel(result['person_id'])
-            print(wantedLevel)
+            wantedLevel = getWantedLevel(result['license_plates'])
 
             result['sensor_id'] = report['sensor_id']
             result['timestamp'] = report['timestamp']
@@ -63,6 +63,7 @@ def addToSuspectTable(report):
                     "actions": result['actions'],
                     "license_plates": result['license_plates'],
                     # "car_model": "", # we dont have that field
+                    "work_visa": result['work_visa'],
                 }
 
                 columns = wantedRecord.keys()
@@ -87,25 +88,15 @@ def sendLicensePlate(lp):
     # res = requests.post(url, json=request)
     # return res.text
 
-
-def getWantedLevel(personId):
+def getWantedLevel(licencePlate):
     KEY = 'duhcEFRU8BhZSZbwonbsVWwUOczcn4O=qFCpOoZDjj4X0bn4TEJyhak44jnxOuFFnTj1G1?d04LxsUmva8f-N-46b=Y4F5aCMeTq?OCfngG7DTwz/X-S-luxTO?yQNuX7/22lgo5JFTPX?0S2eWku?HEsD7RRxdrIFAc6!uAg?JE0DlmGL?/G5ZX?bC05ozSB0XtL1yGILCUihJc22EIYNYPB/DOg!0OrfIgseh58s6WdZg6KvK!xKo6n=!DyUFm'
     headers = {'authorize': KEY}
 
-    r = requests.get("http://10.11.30.212:8000/citizens/tz/" + str(personId), headers=headers)
+    req = requests.get("http://10.11.30.212:8000/citizens/lz/" + str(licencePlate), headers=headers)
 
-    return int(r.text)
-
+    return req.text
 
 def listenForReports():
-    # exampleReport = {
-    #     'number': '80-UJI-95',
-    #     'sensor_id': 'sensor',
-    #     'timestamp': datetime.datetime.now(),
-    #     'sensor_location_x': 1,
-    #     'sensor_location_y': 100
-    # }
-
     report = {}
 
     server_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
@@ -113,13 +104,4 @@ def listenForReports():
 
     while True:
         message, address = server_socket.recvfrom(1024)
-
         addToSuspectTable(message.decode())
-
-
-if __name__ == '__main__':
-    reportsThread = threading.Thread(target=listenForReports)
-    reportsThread.start()
-
-
-
