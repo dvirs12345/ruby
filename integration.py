bfd88df595f65d98cc6395b2b720361fa03a57fd
Shragagg <127030480+Shragagg@users.noreply.github.com>
2023-03-08 09:28:01 +0200

    Update integration.py

--- integration.py
+++ integration.py
@@ -3,6 +3,9 @@ import readLicenecePlate
 import psycopg2
 import threading
 
+#reportsThread = threading.Thread(target=add_to_suspects_table.listenForReports())
+#reportsThread.start()
+
 connection = psycopg2.connect(
     database="postgres",
     user='postgres',
@@ -16,33 +19,27 @@ cursor = connection.cursor()
 cursor.execute(str("SELECT * FROM sensors.images"))
 data = cursor.fetchall()
 
-def addSuspectThread(image, id):
-    open("./2/{}.jpg".format(id), 'wb').write(image[-2])
-    number_plate = readLicenecePlate.getLicensePlate(id)
+threads = []
+
+def addSuspect(image):
+    open("FromDB.jpg", 'wb').write(image[-2])
+    number_plate = readLicenecePlate.getLicensePlate()
     print(number_plate)
     report = {
         'number': number_plate,
-        'sensor_id': image[0],
+        'sensor_id': image[1],
         'timestamp': image[-1],
-        'sensor_location_x': image[1],
-        'sensor_location_y': image[2]
+        'sensor_location_x': image[2],
+        'sensor_location_y': image[3]
     }
 
-    add_to_suspects_table.addToSuspectTable(report)
-
-threads = []
-imageId = 0
-for image in data:
-    addSuspect = threading.Thread(target=addSuspectThread, args=(image, imageId))
+    addSuspect = threading.Thread(target=add_to_suspects_table.addToSuspectTable, args=(report, ))
     threads.append(addSuspect)
     addSuspect.start()
-    imageId += 1
+
+for image in data:
+    addSuspect(image)
 
 for currentThread in threads:
     print(currentThread)
     currentThread.join()
-
-
-
-
-
